8d4a696b745334d608ec1aa544ff29a9314ef8a7
Iddan Aharonson <mail@iddan.co>
2018-03-22 13:18:24 +0200

    Added flow types to typography

--- src/Typography.js
+++ src/Typography.js
@@ -1,84 +1,87 @@
+// @flow
+
 import React from "react";
 import classnames from "classnames";
 import "@material/typography/dist/mdc.typography.css";
+import type { ClassNamed } from './util'
 
-export const Typography = ({ className, ...rest }) => (
+export const Typography = ({ className, ...rest } : ClassNamed) => (
   <span {...rest} className={classnames("mdc-typography", className)} />
 );
 
-export const Display4 = ({ className, ...rest }) => (
+export const Display4 = ({ className, ...rest } : ClassNamed) => (
   <span
     {...rest}
     className={classnames("mdc-typography--display4", className)}
   />
 );
 
-export const Display3 = ({ className, ...rest }) => (
+export const Display3 = ({ className, ...rest } : ClassNamed) => (
   <span
     {...rest}
     className={classnames("mdc-typography--display3", className)}
   />
 );
 
-export const Display2 = ({ className, ...rest }) => (
+export const Display2 = ({ className, ...rest } : ClassNamed) => (
   <span
     {...rest}
     className={classnames("mdc-typography--display2", className)}
   />
 );
 
-export const Display1 = ({ className, ...rest }) => (
+export const Display1 = ({ className, ...rest } : ClassNamed) => (
   <span
     {...rest}
     className={classnames("mdc-typography--display1", className)}
   />
 );
 
-export const Headline = ({ className, ...rest }) => (
+export const Headline = ({ className, ...rest } : ClassNamed) => (
   <span
     {...rest}
     className={classnames("mdc-typography--headline", className)}
   />
 );
 
-export const Title = ({ className, ...rest }) => (
+export const Title = ({ className, ...rest } : ClassNamed) => (
   <span {...rest} className={classnames("mdc-typography--title", className)} />
 );
 
-export const Subheading2 = ({ className, ...rest }) => (
+export const Subheading2 = ({ className, ...rest } : ClassNamed) => (
   <span
     {...rest}
     className={classnames("mdc-typography--subheading2", className)}
   />
 );
 
-export const Subheading1 = ({ className, ...rest }) => (
+export const Subheading1 = ({ className, ...rest } : ClassNamed) => (
   <span
     {...rest}
     className={classnames("mdc-typography--subheading1", className)}
   />
 );
 
-export const Body2 = ({ className, ...rest }) => (
+export const Body2 = ({ className, ...rest } : ClassNamed) => (
   <span {...rest} className={classnames("mdc-typography--body2", className)} />
 );
 
-export const Body1 = ({ className, ...rest }) => (
+export const Body1 = ({ className, ...rest } : ClassNamed) => (
   <span {...rest} className={classnames("mdc-typography--body1", className)} />
 );
 
-export const Caption = ({ className, ...rest }) => (
+export const Caption = ({ className, ...rest } : ClassNamed) => (
   <span
     {...rest}
     className={classnames("mdc-typography--caption", className)}
   />
 );
 
-export const Button = ({ className, ...rest }) => (
+export const Button = ({ className, ...rest } : ClassNamed) => (
   <span {...rest} className={classnames("mdc-typography--button", className)} />
 );
 
-export const AdjustMargin = ({ className, ...rest }) => (
+export const AdjustMargin = ({ className, ...rest } : ClassNamed) => (
   <span
     {...rest}
     className={classnames("mdc-typography--adjust-margin", className)}
